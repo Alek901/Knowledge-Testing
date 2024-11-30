207ab3ebf67bae38fca00a464a964eb352903286
Alek Gonzalez <Magicguys1@icloud.com>
2024-11-29 17:47:24 -0700

    Added more code to the quiz.cy.tsx
    finished the rest of the code so it would complete the quiz by answering all the questions

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -14,5 +14,12 @@ describe('Quiz Component', () => {
         cy.get('[data-testid="next-button"]').click()
     })
 
-    
-}
+    it('shows final score', () => {
+        cy.mount(<Quiz />)
+        cy.get('[data-testid="answer-option"]').each(($option) => {
+            cy.wrap($option).click()
+            cy.get('[data-testid="next-button"]').click()
+        })
+        cy.get('[data-testid="final-score"]').should('be.visible')
+    })
+})
