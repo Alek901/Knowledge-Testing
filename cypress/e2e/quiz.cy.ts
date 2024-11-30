d5e6389dabaf5c18d58ddb9db8d75ac874da03b7
Alek Gonzalez <Magicguys1@icloud.com>
2024-11-29 17:50:35 -0700

    Added code to the e2e/quiz file
    Started the quiz with the code

--- cypress/e2e/quiz.cy.ts
+++ cypress/e2e/quiz.cy.ts
@@ -2,4 +2,8 @@ describe('Quiz E2E', () => {
     beforeEach(() => {
         cy.visit('/')
     })
+
+    it('completes full quiz flow', () => {
+        cy.get('[data-testid="start-quiz"]').click()
+    })
 })
